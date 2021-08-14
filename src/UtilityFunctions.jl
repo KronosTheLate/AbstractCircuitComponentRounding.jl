c278e6c8fdc18d07ce9a9d5a2e2fba3b44fa2bf1
KronosTheLate <densbal@live.no>
2021-08-14 22:18:00 +0200

    More robust handling of sorting

--- src/UtilityFunctions.jl
+++ src/UtilityFunctions.jl
@@ -1,12 +1,12 @@
 geometric_mean(a::Number, b::Number) = √(a*b)
 
 function find_under_and_over(a::Number, possible_values::Array)
-    issorted(possible_values) && error()
+    possible_series_sorted = sort(possible_values)
     i, j = 1, 2
-        while !(possible_values[i] ≤ a ≤ possible_values[j])
+        while !(possible_series_sorted[i] ≤ a ≤ possible_series_sorted[j])
             i+=1; j+=1
         end
-    return possible_values[i], possible_values[j]
+    return possible_series_sorted[i], possible_series_sorted[j]
 end
 
 function norm_to_between_100_and_1000(val::Number; return_OOM=true)
